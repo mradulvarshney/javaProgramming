3bc222f0cfda057417ac0ba6017e0c069378d5ec
Mradul Varshney <80679669+mradulvarshney@users.noreply.github.com>
2021-09-18 21:11:00 +0530

    Add files via upload
    no search in an array

---
+++ elementSearch.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Array
+{
+	Scanner sc = new Scanner(System.in);
+	int n,t,arr[];
+	public void input()
+	{
+		n = sc.nextInt();
+		arr = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+	}
+	public void search()
+	{
+		t=0;
+		System.out.println("Enter a no. to search");
+		int num = sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			if(num==arr[i])
+				t=i;
+		}
+	}
+	public void show()
+	{
+		if(t==0)
+			System.out.println("-1");
+		else
+			System.out.println("Found at index " + t);
+	}
+}
+class elementSearch
+{
+	public static void main(String args[])
+	{
+		Array obj = new Array();
+		obj.input();
+		obj.search();
+		obj.show();
+	}
+}
