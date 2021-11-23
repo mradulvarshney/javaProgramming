782c03a7cf70179c3492fdb3acdb4397e6395a37
Mradul Varshney <80679669+mradulvarshney@users.noreply.github.com>
2021-11-23 07:43:33 +0530

    Average marks of 2 students

---
+++ exceptionHandling/Average_Marks.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class UserDefinedException extends Exception
+{
+    UserDefinedException(String s)
+    {
+        super(s);
+    }
+}
+class Average_Marks
+{
+    Scanner sc=new Scanner(System.in);
+    String name;
+    int marks[], sum;
+    Average_Marks()throws UserDefinedException,NumberFormatException
+    {
+        sum=0;
+        System.out.println("Enter name of student");
+        name=sc.nextLine();
+        marks=new int[3];
+        System.out.println("Enter marks of "+name);
+        for (int i = 0; i < 3; i++)
+        {
+            marks[i] = Integer.parseInt(sc.next());
+
+            if (marks[i] < 0)
+                throw new UserDefinedException("Marks cannot be Negative");
+            else if (marks[i] > 100)
+                throw new UserDefinedException("Marks cannot be more than 100");
+
+            sum = sum + marks[i];
+        }
+    }
+    public static void main(String[] args)
+    {
+        try
+        {
+            Average_Marks stu1=new Average_Marks();
+            System.out.println("Average marks of "+stu1.name+" = "+stu1.sum/3.0);
+            Average_Marks stu2=new Average_Marks();
+            System.out.println("Average marks of "+stu2.name+" = "+stu2.sum/3.0);
+        }
+        catch (UserDefinedException|NumberFormatException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
